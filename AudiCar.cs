202b0fa3193da6ae645e319602abf46e26b96d55
gitty <g8492961@gmail.com>
2023-10-21 20:05:56 +0300

    add the classes files

---
+++ AudiCar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gvbh
+{
+    internal class AudiCar:Car
+    {
+        public AudiCar(double speed) 
+        :base(speed)
+        {
+        
+        }
+
+        public override void AccelerationSpeed()
+        {
+            base.AccelerationSpeed();
+            base.AccelerationSpeed();
+
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "and my car is Audi";
+        }
+    }
+}
---
+++ LexusCar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gvbh
+{
+    internal class LexusCar:Car
+    {
+        public Boolean IsSongWork { get; set; }
+        public LexusCar(double speed)
+            : base(speed)
+        {
+
+        }
+        public void playSong()
+        {
+            this.IsSongWork = true;
+        }
+        public void pauseSong()
+        {
+            this.IsSongWork = false;
+        }
+        public override string ToString()
+        {
+            return base.ToString() +" " + "and my car is lexus";
+        }
+
+
+
+
+    }
+}
